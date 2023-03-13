073c5d5268b8c1cb672da73ecc3276d762b34df0
Riccardo De Maria <riccardodemaria@gmail.com>
2023-03-13 10:37:02 +0100

    add test

--- tests/test_table.py
+++ tests/test_table.py
@@ -25,13 +25,16 @@ def test_row_selection():
     assert t[:, 1].betx==data['betx'][1]
     assert t[:, [0, 2]].betx[1]==data['betx'][2]
     assert t[:, t.s > 1].betx[1]==data['betx'][t.s > 1][1]
+    assert t[:, t.s > 1,1].betx[0]==data['betx'][t.s > 1][0]
 
+def test_row_selection_names():
     assert t[:, "ip1"].betx[0]==data['betx'][0]
     assert t[:, "ip[23]"].betx[0]==data['betx'][1]
     assert t[:, "ip.*##1"].betx[0]==data['betx'][1]
     assert t[:, "notthere"]._nrows==0
     assert t[:, ["ip1", "ip2"]].betx[0]==data['betx'][0]
 
+def test_row_selection_ranges():
     assert t[:, 1:4:3].betx[0] == data['betx'][1]
     assert t[:, 1.5:2.5:"s"].betx[0] == data['betx'][1]
     assert t[:, "ip1":"ip3"].betx[2] == data['betx'][2]
